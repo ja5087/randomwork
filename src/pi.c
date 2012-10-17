c7216d25c9b367b6e03bb1f92e92fb2867f67a8e
Ja5087 <frandomnumbers@gmail.com>
2012-10-17 15:18:44 +0700

    commit new src

--- src/pi.c
+++ src/pi.c
@@ -6,9 +6,11 @@ int main()
 	printf("so u wan diametr usin pi ar pls entr circum\n");
 	pi = fldpi();
 	scanf("%lf", &amount);
+	while ((amount = getchar()) != '\n' && amount != EOF);
 	der = amount / pi;
 	printf("\n here is ur amount lah \n %lf", der );
-	printf("\n we used dis pi ar %lf" , pi);
+	printf("\n we used dis pi ar %lf \n" , pi);
+	printf("enter any letter to continue\n");
 	getchar();
 	
 }
